a9b6f1b4aaa0d963bbbafc5f73300e1c7228331e
user <pooja.petkar123@gmail.com>
2022-09-02 00:32:16 +0530

    Added TestGit class

---
+++ src/com/testgit/pooja/TestGit.java
@@ -0,0 +1,17 @@
+package com.testgit.pooja;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		/**
+		 * This is the new class we have added.
+		 * Now we will commit this class and see if it gets reflected in the git.
+		 */
+		
+		System.out.println("Aagau Pooja");
+		
+		//wait I will commit ok?
+		
+	}
+
+}
