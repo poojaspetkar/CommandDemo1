b379a7b362961024c68cfdc7481d3c7cb8fa2ef0
unknown <pooja.petkar123@gmail.com>
2022-09-01 19:06:30 +0530

    Login page Test case is added

---
+++ src/LoginPage/LoginPageTest.java
@@ -0,0 +1,10 @@
+package LoginPage;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		System.out.println("user is on login page");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitRespProject2 {
+}
